97fd740b6bda0ceea74b39e34250677b0436f0cb
Shiona B <56573375+shionaB@users.noreply.github.com>
2020-02-28 16:14:08 +0530

    Add files via upload

---
+++ Libraries.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from math import sqrt
+from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+print("Hello")
